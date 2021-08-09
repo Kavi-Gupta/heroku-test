42c142eb5a6c828df1409530cd3b8a45fb0eb6fe
Kavi-Gupta <69233899+Kavi-Gupta@users.noreply.github.com>
2021-08-09 15:33:29 -0700

    Update app.js

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ app.get("/", (req, res) => {
     res.send("HELLO");
   });
 
-  app.get("/style.css", (req, res) => {
-    // Display checkout page
-    const path = resolve("./style.css");
-    res.sendFile(path);
-  });
-
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
