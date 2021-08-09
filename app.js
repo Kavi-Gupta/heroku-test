68a000b6bcbffac8372ef2162b96d4bdcf5edfb7
Kavi-Gupta <69233899+Kavi-Gupta@users.noreply.github.com>
2021-08-09 15:10:00 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const { resolve } = require("path");
+
+app.get("/", (req, res) => {
+    // Display checkout page
+    const path = resolve("./index.html");
+    res.sendFile(path);
+  });
+
+  app.get("/style.css", (req, res) => {
+    // Display checkout page
+    const path = resolve("./style.css");
+    res.sendFile(path);
+  });
+
+app.listen(3000, () => console.log(`Node server listening on port ${3000}!`));
